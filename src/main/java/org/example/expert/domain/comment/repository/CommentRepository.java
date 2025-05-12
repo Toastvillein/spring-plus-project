e9ce58bff9c71f0a055f88ad45347ffba624bba1
Toastvillein <okkcba7899@gmail.com>
2025-05-12 17:27:59 +0900

    필수레벨 7

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,12 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    /* 필수레벨 7 N+1 문제 FETCH JOIN 사용
+    *  페이징 처리를 해야할 시엔 @EntityGraph 사용
+    *  @EntityGraph(attributePaths = "user")
+    *  @Query("SELECT c FROM Comment c WHERE c.todo.id = :todoId")
+    *  Page<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId, Pageable pageable);
+    * */
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
