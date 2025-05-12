31dc539c8cffef44873bacf006395feade4943e5
Toastvillein <okkcba7899@gmail.com>
2025-05-12 12:19:41 +0900

    필수레벨 5

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -18,7 +19,11 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
+    /* changeUserRole()의 실행전에 로그가 출력되야 하니
+    * @After 어노테이션을 @Before 로 변경
+    * 또한 잘못된 디렉토리 주소를 변경
+    * */
+    @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole())")
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
